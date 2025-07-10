26047fb01155b5c70c9cf6b253de805b27dd81df
Secure000000 <s.e.c.u.r.e.00000000000000@gmail.com>
2025-07-10 13:47:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 # Simple route to check if the API is working
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify(message="Welcome to the BloxAPI!")
+    return jsonify(message="Welcome to: bloxAPI!")
